315dfbbdb4da7a5a03cd6c4e3a745839cb3a34f9
ImSwampy <tijaguar44@gmail.com>
2024-07-05 13:46:24 +0200

    creating hash function

---
+++ cmake-build-debug/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/InnerNode.cpp.obj"
+  "CMakeFiles/test.dir/InnerNode.cpp.obj.d"
+  "CMakeFiles/test.dir/Nodes.cpp.obj"
+  "CMakeFiles/test.dir/Nodes.cpp.obj.d"
+  "CMakeFiles/test.dir/main.cpp.obj"
+  "CMakeFiles/test.dir/main.cpp.obj.d"
+  "libtest.dll.a"
+  "test.exe"
+  "test.exe.manifest"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
+#include <any>
 #include <iostream>
 #include <bitset>
 #include <list>
 #include "InnerNode.h"
 
+void fill_hash(std::bitset<8> *string[256]) {
+
+    std::bitset<8> swampy_db[8] = {
+        0b01110011, 0b01110111, 0b01100001, 0b01101101, 0b01110000, 0b01111001, 0b01100100, 0b01100010
+    };
+
+    for (int i = 0; i < 256; i++) {
+        string[i] = &swampy_db[i % 8];
+    }
+};
+
+void add_content_to_hash(std::bitset<8> *hash[256], const std::string &content) {
+
+}
+
 int main() {
-    //std::string str;
-    //std::getline(std::cin, str);
-    std::bitset<8> final_string[256];
-    std::bitset<8> swampy_db[8] = {0b01110011, 0b01110111, 0b01100001, 0b01101101, 0b01110000, 0b01111001, 0b01100100, 0b01100010 };
+    std::bitset<8> *final_string[256];
 
-    std::cout << swampy_db[0] << std::endl;
+    fill_hash(final_string);
 
-    for (int i = 0; i < 256; i+=8) {
-        for (int j = 0; j < 8; j++) {
-            final_string[j] = swampy_db[j];
-            std::cout << swampy_db[j] << " ";
-        }
+    for (int i = 0; i < 256; i++) {
+        std::cout << *final_string[i] << " ";
     }
 }
